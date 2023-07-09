c2d0eb59d7656eeda3e8e6d23c3baa885138986f
Benjamin Toh <toh.benjamin123@gmail.com>
2023-07-09 23:47:11 +0800

    remove skills list

--- src/components/ExperienceBlock.js
+++ src/components/ExperienceBlock.js
@@ -13,22 +13,6 @@ function ExperienceBlock({ exp }) {
                 <li key={index} className=''><span className='font-bold text-[#89929b] pr-2'>{`>`}</span> {elem}</li>
             ))}
             </ul>
-            <div className='mt-3 flex flex-col justify-start '>
-                {Object.entries(exp.skills).map(([category, tools])=> (
-                    <div className='flex flex-col' key={category} >
-                        <div className='flex md:flex-row sm:flex-row flex-col'>
-                            <h2 className='font-bold w-28 b'>{category}</h2>
-                            <div className='flex flex-row justify-start flex-wrap'>
-                                {tools.map((tool, index) => (
-                                    <p className='px-1 first:pr-1 first:px-0 whitespace-nowrap' key={tool}>{tool}{index !== tools.length - 1 && " •"}</p>
-                                    
-                                ))}
-                            </div>
-                        </div>
-                        
-                    </div>
-                ))}
-            </div>
         </div>
 
     </div>
