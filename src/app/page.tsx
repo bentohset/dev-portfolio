ffdd25f3af7f4186b92e4bec416637941200d08b
bentohset <toh.benjamin123@gmail.com>
2024-02-20 00:31:10 +0800

    sample 3js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image";
+"use client";
 
+import { Model } from '@/features/model';
 
 // TODO: add a custom loader with my icon
 
 // TODO: threejs
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <div className="">
-       Test
-      </div>
+    <main style={{ width: '100vw', height: '80vh' }}>
+       <Model />
     </main>
   );
 }
