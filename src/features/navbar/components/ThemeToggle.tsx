f934c5c97b77173a11740a0d58cdcb834daefeea
bentohset <toh.benjamin123@gmail.com>
2024-03-01 23:40:54 +0800

    fixes

--- src/features/navbar/components/ThemeToggle.tsx
+++ src/features/navbar/components/ThemeToggle.tsx
@@ -21,15 +21,15 @@ const ThemeToggle = (props: Props) => {
 
   return (
     <button
-      className='rounded-full bg-white px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800 dark:ring-white/10 dark:hover:ring-white/20'
+      className='rounded-full bg-white px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:bg-zinc-800 dark:ring-white/10 dark:hover:ring-white/20'
       onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
     >
       <span className='dark:text-zinc-200 text-zinc-400'>
       {
         resolvedTheme === "dark" ? (
-          <Moon size={20}/>
+          <Moon size={20} className='dark:text-zinc-200 text-zinc-400'/>
         ) : (
-          <Sun size={20}/>
+          <Sun size={20} className='dark:text-zinc-200 text-zinc-400'/>
         )
       }
       </span>
