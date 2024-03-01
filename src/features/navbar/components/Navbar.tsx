ef27373ee0eec225c13b25431fbb201b5ae953a5
bentohset <toh.benjamin123@gmail.com>
2024-03-01 23:40:54 +0800

    fixes

--- src/features/navbar/components/Navbar.tsx
+++ src/features/navbar/components/Navbar.tsx
@@ -3,8 +3,6 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { Disclosure } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
@@ -12,13 +10,11 @@ import ThemeToggle from "./ThemeToggle";
 import { navigation } from "@/lib/data";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
-  DialogDescription,
   DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 
 export function Navbar() {
@@ -69,34 +65,33 @@ export function Navbar() {
                         <button className="rounded-full bg-white px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:bg-zinc-800 dark:ring-white/10 dark:hover:ring-white/20">
                           <Menu
                             size={20}
-                            className="block dark:text-zinc-200 text-zinc-400 "
+                            className="dark:text-zinc-200 text-zinc-400"
                             aria-hidden="true"
                           />
                         </button>
                       </DialogTrigger>
                       <DialogContent className="rounded-2xl w-5/6 h-fit top-44">
                         <DialogHeader>
-                          <DialogDescription>
-                            <div className="flex flex-col w-full text-left justify-center items-center space-y-2 font-medium text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
-                              {navigation.map((item) => (
-                                <Link
-                                  key={item.name}
-                                  href={item.href}
-                                  className={cn(
-                                    path === item.href
-                                      ? "text-devPrimary"
-                                      : "hover:text-devPrimary",
-                                    "rounded-md px-3 py-2 text-md w-full self-center"
-                                  )}
-                                  aria-current={
-                                    path === item.href ? "page" : undefined
-                                  }
-                                >
-                                  {item.name}
-                                </Link>
-                              ))}
-                            </div>
-                          </DialogDescription>
+                          <div className="flex flex-col w-full text-left justify-center items-center space-y-2 font-medium text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+                            {navigation.map((item) => (
+                              <DialogClose asChild key={item.name}>
+                              <Link
+                                href={item.href}
+                                className={cn(
+                                  path === item.href
+                                    ? "text-devPrimary"
+                                    : "hover:text-devPrimary",
+                                  "rounded-md px-3 py-2 text-md w-full self-center"
+                                )}
+                                aria-current={
+                                  path === item.href ? "page" : undefined
+                                }
+                              >
+                                {item.name}
+                              </Link>
+                              </DialogClose>
+                            ))}
+                          </div>
                         </DialogHeader>
                       </DialogContent>
                     </Dialog>
