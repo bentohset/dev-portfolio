e218927df699336cdd6b85ff6855c498583147e6
bentohset <toh.benjamin123@gmail.com>
2024-03-01 16:54:54 +0800

    fix mounting

--- src/features/about/components/SkillCard.tsx
+++ src/features/about/components/SkillCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image';
 import { useMediaQuery } from 'usehooks-ts'
 
@@ -21,13 +21,16 @@ type Props = {
 }
 
 export const SkillCard = (props: Props) => {
-  const { resolvedTheme } = useTheme();
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   const matches = useMediaQuery('(min-width: 768px)');
   const iconSize = matches ? 40 : 26;
 
+  useEffect(() => setMounted(true), []);
+
   const iconSVG = (skill: string): string => {
     if (typeof iconMap[skill] === "object") {
-      if (resolvedTheme === "dark") {
+      if (theme === "dark") {
         return (iconMap[skill] as IconObj).dark;
       } else {
         return (iconMap[skill] as IconObj).default;
@@ -44,7 +47,7 @@ export const SkillCard = (props: Props) => {
       <Tooltip>
         <TooltipTrigger asChild>
           <button className='md:h-24 md:w-24 h-16 w-16 rounded-2xl border border-zinc-100 md:p-6 p-2 dark:border-zinc-700/40 dark:bg-zinc-800 bg-zinc-50'>
-            <Image src={iconSVG(props.skill)} alt="iconskill" height={iconSize} width={iconSize} />
+            {mounted && <Image src={iconSVG(props.skill)} alt="iconskill" height={iconSize} width={iconSize} />}
           </button>
         </TooltipTrigger>
         <TooltipContent>
