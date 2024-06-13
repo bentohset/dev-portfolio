50629ab32340fb7da4ed6204486d0fa16eb06aba
bentohset <toh.benjamin123@gmail.com>
2024-06-13 16:20:12 +0800

    test lazygit

--- src/features/experience/components/AchievementRow.tsx
+++ src/features/experience/components/AchievementRow.tsx
@@ -5,24 +5,24 @@ import { TimelinePoint } from './TimelinePoint';
 type Props = {
   achievement: AchievementType;
 }
-
+// TODO: test
 export const AchievementRow = (props: Props) => {
   const o = props.achievement;
   return (
     <div className='flex flex-row gap-x-3 pr-6'>
       <div>
-        <TimelinePoint image={o.image || "/grey.jpg"}/>
+        <TimelinePoint image={o.image || "/grey.jpg"} />
       </div>
       <div className='w-full'>
         <div className='flex flex-row justify-between w-full items-center'>
           <h1 className='font-semibold text-lg text-devPrimary'>{o.title}</h1>
           <p className='font-medium text-zinc-600 dark:text-zinc-400 text-xs'>{o.period}</p>
-         
+
         </div>
-        
+
         <h2 className='font-medium'>{o.award}</h2>
         <p className='text-zinc-600 dark:text-zinc-400'>{o.desc}</p>
-        
+
       </div>
     </div>
   )
